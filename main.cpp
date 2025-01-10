41232258dc604096230cea68b4ae2ef763074e7b
Kadeska <19519433+kadeska@users.noreply.github.com>
2025-01-09 19:17:27 -0800

    init commit
    This project will probably replace my current 2d game dev project, because i like using qt creator.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "mainwindow.hpp"
+
+#include <QApplication>
+#include <boost/asio.hpp>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
